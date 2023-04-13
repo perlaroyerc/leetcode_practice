e084eab5c56075efafbee19772173d0721c74417
Perla Royer <perlaroyerc@gmail.com>
2023-04-13 13:02:52 -0400

    First attempt

--- Linked List/ReorderList.js
+++ Linked List/ReorderList.js
@@ -1,82 +1,103 @@
+/*
+You are given the head of a singly linked-list. The list can be represented as:
 
-class Node {
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+*/
+
+
+class ListNode {
     constructor(val){
         this.val = val;
         this.next = next;
     }
 }
 
-
-
 const reorderList = (head) => {
-
-    const stack = [];
-
-    let slow = head;
-    let fast = head;
-
-    while(fast && fast.next){
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-
-
-    while(slow){
-        stack.push(slow.val);
-        slow = slow.next;
-    }
-
-    let current = head;
-
-    while(current){
-        let temp = current.next;
-        if(stack.length > 0){
-            const newNode = new ListNode(stack.pop());
-            current.next = newNode;
-            newNode.next = temp;
-            current = temp;
-        }
-    }
-
-    return head;
-
-};
-
-
-
-
-// var reorderList = function(head) {
-
-//     //get the value of n
-//     // let n;
-
-//     // while(head){
-//     //     if(head.next === null) n = head;
-//     //     head = head.next;
-//     // }
-
-//     // const n_node = new ListNode(n);
-//     // let prev = head;
-//     // let current = head.next;
-
-// //while current.next === null
-// //prev.next should point to n
-// //n.next should point to curr
-// //move curr and pointer foward
-// //decrement n
-
-//     // while(current.next){
-//     //     prev.next = n_node;
-//     //     n_node.next = current;
-//     //     prev = curent;
-//     //     current = current.next;
-//     //     n--;
-//     // }
-
-//     // return head;
     
+
+}
+
+//Second attempt using HashMap
+// const reorderList = (head) => {
+//     const hashMap = {};
+//     let length = 0;
+//     let current = head;
+
+//     while(current){
+//         length++;
+//         hashMap[length] = new ListNode(current.val);
+//         current = current.next;
+//     }
+
+//     current = head;
+//     let count = 1;
+
+//     console.log(length);
+//     console.log(hashMap)
+
+//     while(current){
+//         if(count % 2 === 1) {
+//             current.next = hashMap[length];
+//             count++;
+//         } 
+//         else {
+//             current.next = hashMap[count];
+//             length--;
+//             count++;
+//         }
+
+//         current = current.next;
+//     }
+
+//     return head;
+
+// }
+
+
+
+//First attempt using 2 pointers
+// const reorderList = (head) => {
+
+//     const stack = [];
+
+//     let slow = head;
+//     let fast = head;
+
+//     while(fast && fast.next){
+//         slow = slow.next;
+//         fast = fast.next.next;
+//     }
+
+
+//     while(slow){
+//         stack.push(slow.val);
+//         slow = slow.next;
+//     }
+
+//     let current = head;
+
+//     while(current){
+//         let temp = current.next;
+//         if(stack.length > 0){
+//             const newNode = new ListNode(stack.pop());
+//             current.next = newNode;
+//             newNode.next = temp;
+//             current = temp;
+//         }
+//     }
+
+//     return head;
+
 // };
 
 
-//other strategy use a stack to store 2nd half
-//current.next = popped node
+
+
