32a992cd6dadc38726eea3fd1c9cc4f2c0beb57d
Perla Royer <perlaroyerc@gmail.com>
2023-04-20 18:51:59 -0400

    Completed

---
+++ Slow & Fast pointers/MiddleOfALinkedList.js
@@ -0,0 +1,39 @@
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+*/
+
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+//Brute force solution
+//Time complexity: O(n)
+//Space complexity: O(1)
+
+const middleNode = (head) => {
+    let current = head;
+    let len = 0;
+
+    while(current) {
+        len++;
+        current = current.next;
+    }
+
+    current = head;
+    let count = 0;
+
+    while(current) {
+        if(count === Math.floor(len / 2)) return current;
+        count++;
+        current = current.next;
+    }
+}
